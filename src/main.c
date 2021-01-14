acb3c53f20b43447ae5e9438e253be6172ff9843
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-01-14 13:52:35 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,13 +4,12 @@
 #include <keypadc.h>
 #include <compression.h>
 #include <fileioc.h>
-#include <debug.h>
 #include "gfx/DRAAK.h"
 #include "gfx/DRAGAN.h"
 #define APPVAR "DRGNLVL"
 uint24_t about(uint24_t score){
+    uint24_t about2();
     score++;
-    gfx_BlitScreen(); //move the screen to the buffer.
     gfx_SetDrawScreen();
     gfx_FillScreen(255);
     gfx_PrintStringXY("DRAGONCE, a virtual dragon on your calculator!",1,1);
@@ -23,16 +22,38 @@ uint24_t about(uint24_t score){
     gfx_PrintStringXY("-The Cemetech community,",1,131);
     gfx_PrintStringXY("Who helped me with creating this",10,146);
     gfx_PrintStringXY("-Again: Patty van Delft,",1,166);
-    gfx_PrintStringXY("For creating the artwork",10,181);
+    gfx_PrintStringXY("For drawing the dragons",10,181);
     gfx_PrintStringXY("This program was made by: Privacy_Dragon",1,210);
     gfx_PrintStringXY("DEV_ALPHA v0.0.1",200,230);
+    gfx_PrintStringXY(">",5,230);
     do{ //wait untill clear is pressed
         kb_Scan();
+        if (kb_Data[7] == kb_Right){
+            score = about2(score);
+        }
     } while (!kb_IsDown(kb_KeyClear));
     do{//wait untill clear is released
         kb_Scan();
     } while (kb_IsDown(kb_KeyClear));
     gfx_BlitBuffer(); //get the screen back from the buffer, move the about screen in the buffer (though it isn't needed anymore)
+    return score;
+}
+uint24_t about2(uint24_t score){
+    gfx_FillScreen(255);
+    gfx_PrintStringXY("DRAGONCE, a virtual dragon on your calculator!",1,1);
+    gfx_PrintStringXY("Patty van Delft's website:",1,26);
+    gfx_PrintStringXY("https://pattyvandelft.com/",5,41);
+    gfx_PrintStringXY("Celtica Publishing website:",1,61);
+    gfx_PrintStringXY("https://www.celtica-publishing.nl/",5,76);
+    gfx_PrintStringXY("This program was made by: Privacy_Dragon",1,210);
+    gfx_PrintStringXY("DEV_ALPHA v0.0.1",200,230);
+    gfx_PrintStringXY("<",5,230);
+    do{
+        kb_Scan();
+        if (kb_Data[7] == kb_Left){
+            about(score);
+        }
+    } while (kb_Data[6] != kb_Clear);
     return score;
 }
 void feed(){
@@ -116,6 +137,7 @@ uint24_t program_run(uint24_t score){
             care();
         }
         if (kb_Data[1] == kb_Zoom){
+            gfx_BlitScreen(); //move the screen to the buffer.
             score = about(score);
         }
         if (kb_Data[1] == kb_Trace){
@@ -126,39 +148,35 @@ uint24_t program_run(uint24_t score){
 }
 uint24_t OPENVAR(){
     uint24_t score;
-    uint24_t level;
     ti_var_t var;
     ti_var_t slot;
-    score = 100;
     ti_CloseAll();
     if (!ti_Open(APPVAR, "r")){
-        ti_CloseAll();
-         slot = ti_Open(APPVAR, "w");
+        slot = ti_Open(APPVAR, "w");
+        score = 100;
         ti_Write(&score, sizeof(score), 1, slot);
         ti_CloseAll();
     }
-     var = ti_Open(APPVAR, "r");
-    score = ti_Read(&score, sizeof(score), 1, var);
-    dbg_sprintf(dbgout, score);
+    var = ti_Open(APPVAR, "r");
+    ti_Read(&score, sizeof(score), 1, var); //DON'T say score = ti_Read(blabla) that will mess everything up...
     ti_CloseAll();
     if (score < 100){ //if for some reason the score is below 100, which shouldn't happen, reset it to 100
-        printf("%d\n",score);
-        delay(500);
         score = 100;
         var = ti_Open(APPVAR, "w");
         ti_Write(&score, sizeof(score), 1, var);
         }
-    printf("%d",score);
-    delay(500);
+    ti_CloseAll();
+    return score;
+}
+uint24_t UPDATEVAR(uint24_t score){
+    ti_var_t var = ti_Open(APPVAR, "w");
+    ti_Write(&score, sizeof(score), 1, var);
     ti_CloseAll();
     return score;
 }
 int main(void)
 {
-    /*Open an appvar where the score is stored.
-     * If it isn't available, create it and set it to 100.
-     */
-    uint24_t score = OPENVAR();
+    uint24_t score = OPENVAR(); //get the score from the appvar
     uint24_t level = score/100;
     ti_CloseAll();
     if (level > 20){ //If the level is higher than 10, display the big dragon
@@ -169,13 +187,8 @@ int main(void)
         DRAAK_init();
         draak();
     }
-    lines(level); //draw the 'menu' bar
+    lines(level); //draw the rest of the screen
     score = program_run(score);
-    //update the score variable with the new score
-    ti_var_t var = ti_Open(APPVAR, "w");
-    ti_Write(&score, sizeof(score), 1, var);
-    ti_CloseAll();
-    printf("%d",score);
-    delay(500);
+    UPDATEVAR(score); //update the score appvar with the new score
     gfx_End();
 }
