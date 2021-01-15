ba2762b1044934224ea85a0d680be6e32f75e206
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-01-15 17:23:47 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,14 +4,24 @@
 #include <keypadc.h>
 #include <compression.h>
 #include <fileioc.h>
-#include "gfx/DRAAK.h"
-#include "gfx/DRAGAN.h"
+#include "gfx/gfx.h"
 #define APPVAR "DRGNLVL"
-uint24_t about(uint24_t score){
-    uint24_t about2();
+
+uint24_t score;
+uint24_t level;
+
+void setup(){
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+    gfx_SetTransparentColor(148);
+    gfx_FillScreen(224);
+}
+
+void about(){
+    void about2();
     score++;
     gfx_SetDrawScreen();
-    gfx_FillScreen(255);
+    gfx_FillScreen(224);
     gfx_PrintStringXY("DRAGONCE, a virtual dragon on your calculator!",1,1);
     gfx_PrintStringXY("I really want to thank:",1,26);
     gfx_PrintStringXY("-Patty van Delft,",1,46);
@@ -19,7 +29,7 @@ uint24_t about(uint24_t score){
     gfx_PrintStringXY("Which is a incredible serie Patty wrote",10,76);
     gfx_PrintStringXY("-Celtica Publishing,",1,96);
     gfx_PrintStringXY("Who published Dragan Duma",10,111);
-    gfx_PrintStringXY("-The Cemetech community,",1,131);
+    gfx_PrintStringXY("-TimmyTurner51 and others from Cemetech,",1,131);
     gfx_PrintStringXY("Who helped me with creating this",10,146);
     gfx_PrintStringXY("-Again: Patty van Delft,",1,166);
     gfx_PrintStringXY("For drawing the dragons",10,181);
@@ -29,17 +39,17 @@ uint24_t about(uint24_t score){
     do{ //wait untill clear is pressed
         kb_Scan();
         if (kb_Data[7] == kb_Right){
-            score = about2(score);
+            about2();
         }
     } while (!kb_IsDown(kb_KeyClear));
     do{//wait untill clear is released
         kb_Scan();
     } while (kb_IsDown(kb_KeyClear));
     gfx_BlitBuffer(); //get the screen back from the buffer, move the about screen in the buffer (though it isn't needed anymore)
-    return score;
 }
-uint24_t about2(uint24_t score){
-    gfx_FillScreen(255);
+
+void about2(){
+    gfx_FillScreen(224);
     gfx_PrintStringXY("DRAGONCE, a virtual dragon on your calculator!",1,1);
     gfx_PrintStringXY("Patty van Delft's website:",1,26);
     gfx_PrintStringXY("https://pattyvandelft.com/",5,41);
@@ -51,20 +61,35 @@ uint24_t about2(uint24_t score){
     do{
         kb_Scan();
         if (kb_Data[7] == kb_Left){
-            about(score);
+            about();
         }
     } while (kb_Data[6] != kb_Clear);
-    return score;
 }
 void feed(){
-    //this function should display the goat.
-    //and bring it to the dragon...
+    int x, y;
+    score++;
+    gfx_BlitScreen();
+    gfx_SetDrawScreen();
+    for (x = 10; x < 60; x++){
+        gfx_Sprite(goat, x,50);
+        delay(100);
+    }
+    for (y = 50; y < 75; y++){
+        gfx_Sprite(goat, 60, y);
+        delay(100);
+    }
+    gfx_TransparentSprite(goat, 60, 75);
+    gfx_SwapDraw();
+    delay(300);
+    gfx_BlitBuffer();
 }
+
 void care(){
 }
 void train(){
 }
-void lines(uint24_t level) {
+
+void lines() {
     int a;
     gfx_SetColor(255);
     gfx_HorizLine_NoClip(0, 210, 320);
@@ -81,75 +106,37 @@ void lines(uint24_t level) {
     gfx_PrintStringXY("TRAIN",205,223);
     gfx_BlitBuffer();
 }
-void dragan() {
-    unsigned int i;
-    unsigned int x, y;
-    gfx_sprite_t *tile;
-    tile = gfx_MallocSprite(image1_tile_width, image1_tile_height);
-    gfx_Begin();
-    x = 0;
-    y = 0;
-    gfx_SetDrawBuffer();
-    for (i = 0; i < image1_tiles_num; ++i)
-    {
-        zx7_Decompress(tile, image1_tiles_compressed[i]);
-        gfx_Sprite_NoClip(tile, x, y);
 
-        x += image1_tile_width;
-        if (x >= LCD_WIDTH)
-        {
-            y += image1_tile_height;
-            x = 0;
-        }
-    }
-    gfx_SetPalette(globale_palette, sizeof_global_palette, 0);
-    gfx_BlitBuffer();
-}    
-void draak() {
-    unsigned int i;
-    unsigned int x, y;
-    gfx_sprite_t *tile;
-    tile = gfx_MallocSprite(image_tile_width, image_tile_height);
-    gfx_Begin();
-    x = 0;
-    y = 0;
-    gfx_SetDrawBuffer();
-    for (i = 0; i < image_tiles_num; ++i)
-    {
-        zx7_Decompress(tile, image_tiles_compressed[i]);
-        gfx_Sprite_NoClip(tile, x, y);
-
-        x += image_tile_width;
-        if (x >= LCD_WIDTH)
-        {
-            y += image_tile_height;
-            x = 0;
-        }
-    }
-    gfx_SetPalette(global_palette, sizeof_global_palette, 0);
+void drawgan() {
+    gfx_TransparentSprite(dragan, 40, 40);
     gfx_BlitBuffer();
 }
-uint24_t program_run(uint24_t score){
+
+void draak() {
+    gfx_TransparentSprite(dragon, 40, 40);
+    gfx_BlitBuffer();
+}
+
+void program_run(){
     do {
         kb_Scan();
         if (kb_Data[1] == kb_Yequ){
-            feed(); //in this funtion the goat should be displayed.
+            feed();
         }
         if (kb_Data[1] == kb_Window){
             care();
         }
         if (kb_Data[1] == kb_Zoom){
             gfx_BlitScreen(); //move the screen to the buffer.
-            score = about(score);
+            about();
         }
         if (kb_Data[1] == kb_Trace){
             train();
         }
     } while (kb_Data[6] != kb_Clear);
-    return score;
 }
-uint24_t OPENVAR(){
-    uint24_t score;
+
+void OPENVAR(){
     ti_var_t var;
     ti_var_t slot;
     ti_CloseAll();
@@ -163,34 +150,37 @@ uint24_t OPENVAR(){
     ti_Read(&score, sizeof(score), 1, var); //DON'T say score = ti_Read(blabla) that will mess everything up...
     ti_CloseAll();
     if (score < 100){ //if for some reason the score is below 100, which shouldn't happen, reset it to 100
+        printf("%d\n",score);
+        delay(500);
         score = 100;
         var = ti_Open(APPVAR, "w");
         ti_Write(&score, sizeof(score), 1, var);
         }
+    printf("%d",score);
+    delay(500);
     ti_CloseAll();
-    return score;
 }
-uint24_t UPDATEVAR(uint24_t score){
+
+void UPDATEVAR(){
     ti_var_t var = ti_Open(APPVAR, "w");
     ti_Write(&score, sizeof(score), 1, var);
     ti_CloseAll();
-    return score;
 }
+
 int main(void)
 {
-    uint24_t score = OPENVAR(); //get the score from the appvar
-    uint24_t level = score/100;
+    setup();
+    OPENVAR(); //get the score from the appvar
+    level = score/100;
     ti_CloseAll();
     if (level > 20){ //If the level is higher than 10, display the big dragon
-        DRAGAN_init();
-        dragan();
+        drawgan();
     }
     else { //if the level is lower than 10, display the small dragon
-        DRAAK_init();
         draak();
     }
-    lines(level); //draw the rest of the screen
-    score = program_run(score);
-    UPDATEVAR(score); //update the score appvar with the new score
+    lines(); //draw the rest of the screen
+    program_run();
+    UPDATEVAR(); //update the score appvar with the new score
     gfx_End();
 }
