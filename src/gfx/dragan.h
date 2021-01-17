8fd6001952b617d5a1c830b7729ba3e3cbc218a6
Privacy_Dragon <privacy_dragon@tutanota.com>
2021-01-17 16:57:12 +0100

    update files

--- src/gfx/dragan.h
+++ src/gfx/dragan.h
@@ -5,11 +5,11 @@
 extern "C" {
 #endif
 
-#define dragan_width 256
-#define dragan_height 192
-#define dragan_size 49154
+#define dragan_width 146
+#define dragan_height 105
+#define dragan_size 15332
 #define dragan ((gfx_sprite_t*)dragan_data)
-extern unsigned char dragan_data[49154];
+extern unsigned char dragan_data[15332];
 
 #ifdef __cplusplus
 }
